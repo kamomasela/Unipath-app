62eb3a6a66d1536ee4e0a4c96ccd7a9da6a4c08b
Tebogo Msimanga <tebogo_msimanga@MacBookAir.home>
2026-03-17 15:31:45 +0200

    Fix build script: remove pending_rules.json copy (file is gitignored)

--- tools/build_web_assets.sh
+++ tools/build_web_assets.sh
@@ -18,7 +18,6 @@ cp "${ROOT_DIR}/sw.js" "${WWW_DIR}/sw.js"
 cp "${ROOT_DIR}/manifest.webmanifest" "${WWW_DIR}/manifest.webmanifest"
 
 cp "${ROOT_DIR}/data/approved_rules.json" "${WWW_DIR}/data/approved_rules.json"
-cp "${ROOT_DIR}/data/pending_rules.json" "${WWW_DIR}/data/pending_rules.json"
 cp "${ROOT_DIR}/data/approval_decisions.json" "${WWW_DIR}/data/approval_decisions.json"
 
 # Cache-busting token replacement (cross-platform: macOS and Linux)
